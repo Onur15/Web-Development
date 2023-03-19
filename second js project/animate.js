05bf6bf82b65f783fa8547171b8af4c4a0a83e12
Onurcongar <onurcongar@gmail.com>
2023-03-19 16:43:19 +0300

    .

---
+++ second js project/animate.js
@@ -0,0 +1,64 @@
+var ms = 15;
+var t = setInterval(move, ms);
+var pos = 0;
+var value = 1;
+var container = document.getElementById("container");
+var box = document.getElementById("box");
+box.style.top = (container.clientHeight - box.clientHeight) / 2 + "px";
+document.addEventListener('keypress', (event) => {
+    var name = event.key;
+    if (name == "w") {
+        up()
+    }
+    if (name == "s") {
+        down()
+    }
+});
+
+function move() {
+    var width = container.clientWidth;
+    if (pos + value >= width - box.clientWidth) {
+        value *= -1;
+        pos += value;
+        box.style.left = pos + "px";
+    } else if (pos + value > 0 && pos < width - box.clientWidth) {
+        pos += value;
+        box.style.left = pos + "px";
+    } else if (pos + value <= 0) {
+        value *= -1;
+        pos += value;
+        box.style.left = pos + "px";
+    }
+}
+
+function up() {
+    if (ms > 5) {
+        ms -= 5;
+        t = clearInterval(t);
+        t = setInterval(move, ms);
+        console.log(ms);
+    } else if (Math.abs(value) < box.clientWidth / 2) {
+        value += 1 * Math.sign(value);
+        console.log(`Value: ${Math.abs(value)}`);
+    }
+}
+
+function down() {
+    if (Math.abs(value) > 1) {
+        value -= 1 * Math.sign(value);
+        console.log(`Value: ${Math.abs(value)}`);
+    } else {
+        ms += 5;
+        t = clearInterval(t);
+        t = setInterval(move, ms);
+        console.log(ms);
+    }
+}
+
+function stop() {
+    if (t != undefined) {
+        t = clearInterval(t);
+    } else {
+        t = setInterval(move, ms);
+    }
+}
